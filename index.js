dd5d5373e290f4b1089b21a23c9a2fc1f3d6e77e
Mayur Shivakumar <1616.mayur@gmail.com>
2024-03-15 00:46:31 -0700

    Update PORT variable value

--- index.js
+++ index.js
@@ -96,7 +96,7 @@ app.use((req, res) => {
   });
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
  console.log(`Gateway is running on port ${PORT}`);
